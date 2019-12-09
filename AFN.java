6ce2f85f0d28ab12096b1b100c3244f1846b992e
AxelGarciaTello <gata21022000@hotmail.com>
2019-12-09 17:22:14 -0600

    Inicio del programa

---
+++ AFN.java
@@ -0,0 +1,24 @@
+/*
+ * Instituto Politecnico Nacional
+ * Escuela Superior de Computo
+ * Teoria computacional
+ * Grupo 2CV1
+ * Alumno: García Tello Axel
+ * Profesor: Benjamin Luna Benoso
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * De AFN a AFD
+ *
+ * Fecha: 5 de septiembre de 2019
+*/
+
+import java.io.IOException;
+
+
+public class AFN {
+
+    public static void main(String[] args) throws IOException{
+        Programa inicio=new Programa();
+        inicio.iniciar();
+    }
+    
+}
